b1e533d29c62f0a6bb15d31087b4d0ba29fdb2d4
Greg PFISTER <54026632+gpfister@users.noreply.github.com>
2023-06-10 18:47:45 +0200

    Bug/20 (#23)
    * Build a specific version of the image and image cleanup
    
    * Fix pull_request.yaml
    
    ---------
    
    Co-authored-by: Greg PFISTER <no-reply@mail.com>

--- src/scripts/buildx/push.sh
+++ src/scripts/buildx/push.sh
@@ -25,7 +25,6 @@ VERSION_MAJOR=$(echo $VERSION | sed 's/\([0-9]*\).\([0-9]*\).\([0-9]*\)$/\1/')
 VERSION_MINOR=$(echo $VERSION | sed 's/\([0-9]*\).\([0-9]*\).\([0-9]*\)$/\1.\2/')
 DOCKERFILE=$(echo "./Dockerfile."$1)
 IMAGE_NAME=$(cat .image_name)
-IMAGE="$IMAGE_NAME:$1"
 IMAGE_VERSION="$IMAGE_NAME:$1-$VERSION"
 IMAGE_VERSION_MAJOR="$IMAGE_NAME:$1-$VERSION_MAJOR"
 IMAGE_VERSION_MINOR="$IMAGE_NAME:$1-$VERSION_MINOR"
@@ -36,25 +35,10 @@ if [ ! -f "$DOCKERFILE" ]; then
 fi
 
 docker buildx build --push \
-                    --platform linux/arm64,linux/amd64 \
-                    -t $IMAGE \
+                    --platform linux/arm64,linux/amd64,linux/arm/v7 \
                     -t $IMAGE_VERSION \
                     -t $IMAGE_VERSION_MAJOR \
                     -t $IMAGE_VERSION_MINOR \
-                    -f "$DOCKERFILE" .
-docker buildx build --push \
-                    --platform linux/arm64 \
-                    -t $IMAGE-arm64 \
-                    -t $IMAGE_VERSION-arm64 \
-                    -t $IMAGE_VERSION_MAJOR-arm64 \
-                    -t $IMAGE_VERSION_MINOR-arm64 \
-                    -f "$DOCKERFILE" .
-docker buildx build --push \
-                    --platform linux/amd64 \
-                    -t $IMAGE-amd64 \
-                    -t $IMAGE_VERSION-amd64 \
-                    -t $IMAGE_VERSION_MAJOR-amd64 \
-                    -t $IMAGE_VERSION_MINOR-amd64 \
                     -f "$DOCKERFILE" .
 
 # End
